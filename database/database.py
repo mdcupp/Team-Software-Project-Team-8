c3c89ca4f8b48b36f86084d35370404abd77c3b4
csrosin <156369815+csrosin@users.noreply.github.com>
2026-03-14 19:39:42 -0400

    add reaction table + related methods
    heavily modeled after message ones

--- database/database.py
+++ database/database.py
@@ -8,6 +8,7 @@ class Database():
         self.cursor = self.con.cursor()
 
         self.createMessageTable()
+        self.createReactionTable()
 
         
     # Creates the message table ONLY IF IT DOES NOT EXIST
@@ -47,4 +48,45 @@ class Database():
     
         count = result[0] if result else 0
     
+        return count
+
+    # Create reaction table if it doesn't exist
+    def createReactionTable(self):
+        self.cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS reactions(emoji, sender, receiver, message_id, time);
+                            """)
+
+        print("LOG: Reactions Table created if not already existing")
+
+    # Reset reaction table
+    def resetReactionTable(self):
+        self.cursor.executescript("""
+                                  DROP TABLE IF EXISTS reactions;
+                                  CREATE TABLE IF NOT EXISTS reactions(emoji, sender, receiver, message_id, time);
+                                  """)
+        self.con.commit()
+
+        print("LOG: Reactions Table reset")
+
+    # Insert log of reaction into table
+    def insertReaction(self, emoji, sender_id, receiver_id, message_id):
+        time = datetime.now()
+
+        self.cursor.execute("""
+                            INSERT INTO reactions VALUES(?, ?, ?, ?, ?);
+                            """, (emoji, sender_id, receiver_id, message_id, time))
+
+        self.con.commit()
+
+        print(f"LOG: Reaction {emoji} sent by {sender_id} to {receiver_id}")
+
+    # Get reaction count for a given user
+    def getReactionCount(self, author_id):
+        result = self.cursor.execute("""
+                                     SELECT COUNT(*) FROM messages WHERE author = ?;
+                                     """, (author_id,))
+        result = self.cursor.fetchone()
+
+        count = result[0] if result else 0
+
         return count
